4124ceb6cc7b18b5f0526c0311364408f3c4f5f5
xiaobingo <xiaobingo@w520.(none)>
2013-07-11 12:03:57 -0500

    update README on how to use MPI as protocol

--- src/comp.sh
+++ src/comp.sh
@@ -1,9 +1,9 @@
-mpicxx mpi_broker.cpp proxy_stub.cpp mq_proxy_stub.cpp ipc_plus.cpp mpi_proxy_stub.cpp Util.cpp HTWorker.cpp Const.cpp novoht.cpp meta.pb.cc -DMPI_INET -lprotobuf -o mpibroker
-echo "mpibroker compiled"
+mpicxx mpi_broker.cpp proxy_stub.cpp mq_proxy_stub.cpp ipc_plus.cpp mpi_proxy_stub.cpp Util.cpp HTWorker.cpp Const.cpp novoht.cpp meta.pb.cc -DMPI_INET -lprotobuf -o zht-mpibroker
+echo "zht-mpibroker compiled"
 
 
-mpicxx ZHTServer.cpp mpi_server.cpp ProxyStubFactory.cpp proxy_stub.cpp mpi_proxy_stub.cpp Util.cpp mq_proxy_stub.cpp ipc_plus.cpp ConfHandler.cpp ConfEntry.cpp StrTokenizer.cpp HTWorker.cpp Const.cpp novoht.cpp meta.pb.cc -DMPI_INET -lprotobuf -o mpiserver
-echo "mpiserver compiled"
+mpicxx ZHTServer.cpp mpi_server.cpp ProxyStubFactory.cpp proxy_stub.cpp mpi_proxy_stub.cpp Util.cpp mq_proxy_stub.cpp ipc_plus.cpp ConfHandler.cpp ConfEntry.cpp StrTokenizer.cpp HTWorker.cpp Const.cpp novoht.cpp meta.pb.cc -DMPI_INET -lprotobuf -o zht-mpiserver
+echo "zht-mpiserver compiled"
 
-#mpiexec -np 4 ./mpiserver -z zht.conf -n neighbor.conf : ./mpibroker
+#mpiexec -np 4 ./zht-mpiserver -z zht.conf -n neighbor.conf : ./zht-mpibroker
 
